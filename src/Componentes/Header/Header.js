5cc7e32b716d0dc3a8782e71d1ea277a5e7b79e7
rashelmahomud <rashelmahmudraj1998@gmail.com>
2022-03-25 23:54:00 +0600

    added setup all

---
+++ src/Componentes/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='header'>Excellent All Watch </h1>
+            <h3 className='header-title'>Chose Your Favorites Watch.</h3>
+        </div>
+    );
+};
+
+export default Header;
